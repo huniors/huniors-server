af1d394cee55b522c396ea873acb534bc55f67f4
Jinhui Kim <202204487@hufs.ac.kr>
2024-06-29 00:29:03 +0900

    feat: 토큰 기능 추가 시도

--- index.js
+++ index.js
@@ -1,25 +1,18 @@
-import express from 'express';      /* HTTP 서버에 들어오는 요청을 처리해주는 핸들러/미들웨어 */
-import cors from 'cors';            /* 클라이언트가 서버에 요청을 보내기 전 서버의 유효성을 검사하기 위해 CORS 라는 사전 요청을 보내는데, 이를 처리하는 미들웨어. */
-import mongoose from 'mongoose';    /* MongoDB 에 저장할 데이터를 객체로 다루고, 데이터의 스키마를 설정하는 기능을 제공하는 라이브러리*/
+import express from 'express';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import loginRouter from './api/login.js';
+import checkTokenRouter from './api/CheckToken.js';
 
 const app = express();
-app.use(express.json());
-app.use(cors({
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
-}));
-app.options('*', cors());
-app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`);
+const PORT = 3000;
+
+app.use(bodyParser.json());
+app.use(cookieParser());  // cookie-parser 미들웨어 설정
+
+app.use('/login', loginRouter);
+app.use('/', checkTokenRouter);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
-
-mongoose.connect(process.env.DBURI)
-    .then(() => console.log('MongoDB connected'))
-    .catch(err => console.log(err));
-
-import checkTokenApi from './api/CheckToken.js';
-import signInApi from './api/SignIn.js';
-import signUpApi from './api/SignUp.js';
-
-app.use('/api/checktoken', checkTokenApi);
-app.use('/api/signin', signInApi);
-app.use('/api/signup', signUpApi);
